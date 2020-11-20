649b3d58b4b88c51b6c8db9c3f91580cec884e3b
December81 <megastudio@outlook.fr>
2020-11-20 09:51:04 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Je cible le bouton caddie
+let cad = document.getElementById("cad");
+// let player = document.getElementById("audiotag1");
+
+cad.addEventListener("click", function (event) {
+   
+
+});
